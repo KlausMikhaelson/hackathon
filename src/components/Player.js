faf1f7ae9f95b66e3b6e589a934a193fdca2a498
KlausMikhaelson <satyamsingh5076@gmail.com>
2023-02-05 12:54:58 +0530

    fixed physics for player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,14 +1,23 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as THREE from "three";
 import { useLoader } from "@react-three/fiber";
+import { useBox } from "@react-three/cannon";
 
 const PlayerModel = () => {
+    const [ref] = useBox(() => ({
+        mass: 1,
+        type: "Dynamic",
+        position: [4, 8, 3],
+        args: [0.5,0.5,0.5]
+    }))
     const modelPlayer = useLoader(GLTFLoader, "/Burger.glb")
     return(
         <>
+        <mesh ref={ref}>
         <object3D>
             <primitive object={modelPlayer.scene} />
         </object3D>
+        </mesh>
         </>
     )
 }
